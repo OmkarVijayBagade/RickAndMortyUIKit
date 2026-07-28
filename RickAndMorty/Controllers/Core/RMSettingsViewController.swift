b6df6162ebed979d84a25becc2c1fc126a2bdceb
Omkar Vijay Bagade <omkarbagade16@gmail.com>
2026-07-28 22:31:34 +0530

    Created Rick and Morty app

---
+++ RickAndMorty/Controllers/Core/RMSettingsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  RMSettingsViewController.swift
+//  RickAndMorty
+//
+//  Created by Omkar Vijay Bagade on 28/07/26.
+//
+
+import UIKit
+
+final class RMSettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+        
+        title = "Settings"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ RickAndMorty/Models/RMLocation.swift
@@ -0,0 +1,8 @@
+//
+//  RMLocation.swift
+//  RickAndMorty
+//
+//  Created by Omkar Vijay Bagade on 28/07/26.
+//
+
+import Foundation
